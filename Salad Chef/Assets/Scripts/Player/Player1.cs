f4dc4a8e9d75c8a1cb5f40f461caebe8f9338f66
DhumilDholiya <30365717+DhumilDholiya@users.noreply.github.com>
2019-05-14 21:20:59 +0530

    Added Player's Movement
    added individual player movement and basic prototype for level desgning

---
+++ Salad Chef/Assets/Scripts/Player/Player1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+    private float speed = 2f;
+    private bool canMove = true;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //check for Timeout
+    }
+
+    void FixedUpdate()
+    {
+        //move player
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        float h = Input.GetAxisRaw("Horizontal1");
+        float v = Input.GetAxisRaw("Vertical1");
+
+        Vector2 movementVector = new Vector2(h, v);
+        rb.velocity = movementVector.normalized * speed ;
+        Debug.Log("Player 1 ::" + rb.velocity);
+    }
+
+}
---
+++ Salad Chef/Assets/Scripts/Player/Player2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+
+    private float speed = 2f;
+    private bool canMove = true;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //check for Timeout
+    }
+
+    void FixedUpdate()
+    {
+        //move player
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        float h = Input.GetAxisRaw("Horizontal2");
+        float v = Input.GetAxisRaw("Vertical2");
+
+        Vector2 movementVector = new Vector2(h, v)  ;
+        rb.velocity = movementVector.normalized * speed;
+        Debug.Log("Player 2 ::" + rb.velocity);
+    }
+}
