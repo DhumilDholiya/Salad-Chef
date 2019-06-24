75a026d33cf3d8a0ee69b71c1f5b328ef0688079
DhumilDholiya <30365717+DhumilDholiya@users.noreply.github.com>
2019-06-24 22:48:13 +0530

    Implemeted vegetable picking from Tables
    Player can maximum pick up 2 vegetables at a time from tables. and its shows on top of player in which order they picked vegetables.

--- Salad Chef/Assets/Scripts/Player/Player2.cs
+++ Salad Chef/Assets/Scripts/Player/Player2.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player2 : MonoBehaviour {
 
-    private float speed = 2f;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private KeyCode useKey;
     private bool canMove = true;
     private Rigidbody2D rb;
+
+    //UI variables
+    public Text firstVegetable = null;
+    public Text secondVegetable = null;
+
+    //To store current vegetables holder
+    private List<string> vegetablesInHand = new List<string>();
+    //To store chopped vegetables
+    private List<string> choppedVegetables = new List<string>();
 
     void Start()
     {
@@ -39,5 +50,33 @@ public class Player2 : MonoBehaviour {
         Vector2 movementVector = new Vector2(h, v)  ;
         rb.velocity = movementVector.normalized * speed;
         Debug.Log("Player 2 ::" + rb.velocity);
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Vegetables"))
+        {
+            //store that vegetable in list if player doesnt have 2 vegetables in hand.
+            if (vegetablesInHand.Count < 2 && Input.GetKeyDown(useKey))
+            {
+                AddToPlayerHand(other.gameObject);
+            }
+        }
+    }
+
+    void AddToPlayerHand(GameObject curVege)
+    {
+        string vegeName = curVege.gameObject.name;
+        Debug.Log(vegeName);
+        vegetablesInHand.Add(vegeName);
+        //check if it is first vegetable or a second
+        if (vegetablesInHand.Count == 1)
+        {
+            firstVegetable.text = vegeName;
+        }
+        else if (vegetablesInHand.Count == 2)
+        {
+            secondVegetable.text = vegeName;
+        }
     }
 }
